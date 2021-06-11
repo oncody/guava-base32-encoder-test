11a44741ded12342279be94e87352f46a68326af
Cody Smith <37386486+oncody@users.noreply.github.com>
2021-06-10 22:22:56 -0600

    feat: testing guava

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import com.google.common.io.BaseEncoding;
+
+import java.security.SecureRandom;
+
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hi");
+        int BITS_PER_BYTE = 8;
+        int BITS_PER_BASE_32_CHAR = 5;
+
+        SecureRandom randomDataGenerator = new SecureRandom();
+        BaseEncoding base32Encoder = BaseEncoding.base32();
+
+        for(int charCount = 1; charCount <= 20; charCount++) {
+            int byteCount = (int) Math.ceil((1.0 * charCount * BITS_PER_BASE_32_CHAR) / BITS_PER_BYTE);
+            int bitCount = byteCount * BITS_PER_BYTE;
+//            int usableChars = (int) Math.floor((1.0 * bitCount) / BITS_PER_BASE_32_CHAR);
+            System.out.println(String.format("Chars: (%s) Bytes: (%s) Bits: (%s)", charCount, byteCount, bitCount));
+
+            byte[] randomBytes = new byte[byteCount];
+            randomDataGenerator.nextBytes(randomBytes);
+            String randomString = base32Encoder.encode(randomBytes);
+            String trimmedRandomString = randomString.substring(0, charCount);
+            String paddedTrimmedRandomString = randomString.substring(0, charCount) + "E";
+            System.out.println(String.format("Chars: (%s) Bytes: (%s) Bits: (%s) String: (%s) Trimmed: (%s) Padded: (%s)", charCount, byteCount, bitCount, randomString, trimmedRandomString, paddedTrimmedRandomString));
+            byte[] trimmedStringBytes = base32Encoder.decode(paddedTrimmedRandomString);
+        }
     }
 }
